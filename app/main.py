f1bec2b00989db9e8f00a258ed80e92087c59938
Tanesya <tanesya.t@gmail.com>
2025-03-15 08:38:02 +0700

    add upload csv logic

--- app/main.py
+++ app/main.py
@@ -1,3 +1,30 @@
-from fastapi import FastAPI
+import os
+import uuid
+
+from fastapi import FastAPI, File, HTTPException, UploadFile
 
 app = FastAPI()
+
+UPLOAD_DIR = "files"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+
+async def save_uploaded_csv(file: UploadFile, dir: str):
+    name = f"{uuid.uuid4()}_{file.filename}"
+    path = os.path.join(dir, name)
+
+    with open(path, "wb") as f:
+        content = await file.read()
+        f.write(content)
+
+    return path
+
+
+@app.post("/upload-csv/")
+async def create_upload_csv(file: UploadFile = File(...)):
+    if not file.filename.endswith(".csv"):
+        raise HTTPException(status_code=400, detail="Only accept .csv")
+
+    file_path = await save_uploaded_csv(file, dir=UPLOAD_DIR)
+
+    return {"message": "Success", "path": file_path}

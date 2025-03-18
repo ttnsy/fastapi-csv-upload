5ccefb901fb7147b7eef9dbbf18d6d698b6982e2
Tanesya <tanesya.t@gmail.com>
2025-03-18 21:33:54 +0700

    test: add test to download csv

--- tests/test_app.py
+++ tests/test_app.py
@@ -39,3 +39,15 @@ def test_upload_rejects_non_csv(client):
         "/csv-file/", files={"file": ("not_a_csv.txt", fake_file, "text/plain")}
     )
     assert response.status_code == 400
+
+
+def test_download_uploaded_csv_file(client, temp_upload_dir):
+    with open("tests/sample.csv", "rb") as f:
+        response = client.post(
+            "/csv-file/", files={"file": ("sample.csv", f, "text/csv")}
+        )
+    assert response.status_code == 200
+    filename = Path(response.json()["path"]).name
+
+    download_response = client.get(f"/csv-file/{filename}")
+    assert download_response.status_code == 200

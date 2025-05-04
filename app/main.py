2d079850d164405af63991b4f4fe0d65f1b9b8ed
Tanesya <tanesya.t@gmail.com>
2025-05-04 23:18:00 +0700

    feat: initialize database

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
+from fastapi import FastAPI
+from sqlmodel import SQLModel
+
+from .database import engine
 from .routers import csv_file
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(csv_file.router)
 
---
+++ app/models.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timezone
+from typing import Optional
+
+from sqlmodel import Field, SQLModel
+
+
+class CSVMetadata(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name_stored: str = Field(index=True)
+    name_original: str
+    size_bytes: int
+    nrows: int
+    ncols: int
+    uploaded_at: datetime = Field(default_factory=datetime.now(timezone.utc))

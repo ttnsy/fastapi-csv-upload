6b4ca0b6bf091bb0549bb0032c771b280f4b4d0e
Tanesya <tanesya.t@gmail.com>
2025-05-05 15:02:53 +0700

    tests: update upload response with new metadata

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,4 @@
 import io
-from pathlib import Path
 
 
 def test_upload_csv_file(client, temp_upload_dir):
@@ -8,9 +7,18 @@ def test_upload_csv_file(client, temp_upload_dir):
             "/csv-file/", files={"file": ("sample.csv", f, "text/csv")}
         )
     assert response.status_code == 200
-    saved_path = Path(response.json()["path"])
+
+    res = response.json()
+    assert "metadata" in res
+
+    metadata = res["metadata"]
+    assert "name_stored" in metadata
+    assert metadata["name_original"] == "sample.csv"
+    assert metadata["nrows"] == 5
+    assert metadata["ncols"] == 5
+
+    saved_path = temp_upload_dir / metadata["name_stored"]
     assert saved_path.exists()
-    assert saved_path.parent == temp_upload_dir
 
 
 def test_upload_rejects_non_csv(client):
@@ -27,7 +35,8 @@ def test_download_uploaded_csv_file(client, temp_upload_dir):
             "/csv-file/", files={"file": ("sample.csv", f, "text/csv")}
         )
     assert response.status_code == 200
-    filename = Path(response.json()["path"]).name
+    metadata = response.json()["metadata"]
+    filename = metadata["name_stored"]
 
     download_response = client.get(f"/csv-file/{filename}")
     assert download_response.status_code == 200

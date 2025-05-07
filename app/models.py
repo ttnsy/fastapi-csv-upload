ae118bd29f9828afd78b03293f1eceed5d705cfc
Tanesya <tanesya.t@gmail.com>
2025-05-07 17:46:42 +0700

    refactor: remove 'id' from CSVMetadata model & set name_stored as PK

--- app/models.py
+++ app/models.py
@@ -1,12 +1,10 @@
 from datetime import datetime, timezone
-from typing import Optional
 
 from sqlmodel import Field, SQLModel
 
 
 class CSVMetadata(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name_stored: str = Field(index=True)
+    name_stored: str = Field(index=True, primary_key=True)
     name_original: str
     size_bytes: int
     nrows: int

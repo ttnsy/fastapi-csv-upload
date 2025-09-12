b14af1686dcd2783f2396c3414de827de61c0e69
Tanesya <tanesya.t@gmail.com>
2025-09-12 15:03:49 +0700

    feat: initialize logging

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+import json
+import logging
+import logging.config
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -8,6 +11,13 @@ from app.routers import csv_file
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     yield
+
+
+with open("logging.json", "r") as f:
+    config = json.load(f)
+    logging.config.dictConfig(config)
+
+logger = logging.getLogger(__name__)
 
 
 app = FastAPI(lifespan=lifespan)
--- app/routers/csv_file.py
+++ app/routers/csv_file.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 
 from fastapi import APIRouter, File, HTTPException, UploadFile
@@ -6,6 +7,8 @@ from fastapi.responses import FileResponse
 from app.crud import create_metadata
 from app.dependencies import SessionDep, UploadDirDep
 from app.utils.csv_files import extract_csv_metadata, save_uploaded_csv
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter(
     prefix="/csv-file",
@@ -21,10 +24,14 @@ async def upload_csv(
     file: UploadFile = File(...),
 ):
     if file.filename is None:
+        logger.warning("Upload attempt with missing filename")
         raise HTTPException(status_code=400, detail="Filename is missing")
 
     if not file.filename.endswith(".csv"):
+        logger.warning(f"Rejected file {file.filename} (not .csv)")
         raise HTTPException(status_code=400, detail="Only accept .csv")
+
+    logger.info(f"Uploading file {file.filename} to {upload_dir}")
 
     file_path = await save_uploaded_csv(file, dir=upload_dir)
 
@@ -33,9 +40,12 @@ async def upload_csv(
             file_path, name_original=Path(file.filename).stem
         )
         metadata = create_metadata(session=session, csv_metadata=csv_metadata)
+        logger.info(f"Metadata created for {file.filename}: {metadata}")
     except Exception as e:
+        logger.exception(f"Failed processing {file.filename}")  # logs stack trace
         if file_path.exists():
             file_path.unlink()
+            logger.info(f"Deleted incomplete file {file_path}")
         raise HTTPException(status_code=500, detail=f"Upload failed: {str(e)}")
 
     return {"message": "Success", "metadata": metadata}
@@ -46,6 +56,8 @@ async def download_csv(filename: str, upload_dir: UploadDirDep):
     file_path = upload_dir / filename
 
     if not file_path.exists():
+        logger.warning(f"Download failed: {filename} not found in {upload_dir}")
         raise HTTPException(status_code=404, detail="File not found")
 
+    logger.info(f"Serving file {filename} from {upload_dir}")
     return FileResponse(path=file_path, filename=filename, media_type="text/csv")

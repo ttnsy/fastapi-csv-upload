1806e8eaf05c8a29f4b0e2dce5bdf312a8a4dfab
Tanesya <tanesya.t@gmail.com>
2026-06-12 16:03:03 +0700

    refactor: change database config to use DATABASE_URL

--- app/config.py
+++ app/config.py
@@ -6,11 +6,7 @@ UPLOAD_DIR = Path("data")
 
 
 class Settings(BaseSettings):
-    postgres_user: str
-    postgres_password: str
-    postgres_db: str
-    postgres_host: str = "db"
-    postgres_port: int = 5432
+    database_url: str
 
 
 settings = Settings()
--- app/database.py
+++ app/database.py
@@ -1,15 +1,5 @@
-from sqlalchemy import URL
 from sqlmodel import create_engine
 
 from app.config import settings
 
-url = URL.create(
-    drivername = "postgresql+psycopg",
-    username = settings.postgres_user,
-    password = settings.postgres_password,
-    host = settings.postgres_host,
-    port = settings.postgres_port,
-    database = settings.postgres_db
-)
-
-engine = create_engine(url, echo=True)
+engine = create_engine(settings.database_url, echo=True)
